90775e7f79b435cd4b2183160857fc9d2789062a
Sean Matheson <sean@ctrlplusb.com>
2016-04-26 15:37:10 +0100

    FIXED: Errors were thrown for functions with default'ed params and a type   checked return value.

---
+++ test/fixtures/default-with-return-type/actual.js
@@ -0,0 +1,3 @@
+function foo(x = 'foo' : t.String) : t.String {
+  return x;
+}
---
+++ test/fixtures/default-with-return-type/expected.js
@@ -0,0 +1,10 @@
+function foo(x = 'foo': t.String): t.String {
+  t.assert(t.String.is(x));
+
+  var ret = function (x) {
+    return x;
+  }.call(this, x);
+
+  t.assert(t.String.is(ret));
+  return ret;
+}

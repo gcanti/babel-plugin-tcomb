561d09b580ed723169f8aed3f80532c583c17ddf
gcanti <giulio.canti@gmail.com>
2016-09-30 12:33:29 +0200

    annotated functions now handle exact types, fix #127

--- test/fixtures/exact/expected.js
+++ test/fixtures/exact/expected.js
@@ -13,3 +13,40 @@ const B = _t.interface({
   name: "B",
   strict: true
 });
+
+function f(x) {
+  _assert(x, _t.interface({
+    x: _t.String
+  }, {
+    strict: true
+  }), "x");
+}
+function g() {
+  const ret = function () {}.call(this);
+
+  _assert(ret, _t.interface({
+    x: _t.String
+  }, {
+    strict: true
+  }), "return value");
+
+  return ret;
+}
+function h(x) {
+  _assert(x, _t.interface({
+    x: _t.String
+  }, {
+    strict: true
+  }), "x");
+}
+function i() {
+  const ret = function () {}.call(this);
+
+  _assert(ret, _t.interface({
+    x: _t.String
+  }, {
+    strict: true
+  }), "return value");
+
+  return ret;
+}

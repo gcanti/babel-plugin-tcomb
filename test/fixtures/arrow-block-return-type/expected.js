0d47e5138108c5af788725684fa26c8faaea13be
Giulio Canti <giulio.canti@gmail.com>
2016-10-13 15:31:05 +0200

    return types on fat-arrow functions lose access to `this`, fix #134 (#135)

--- test/fixtures/arrow-block-return-type/expected.js
+++ test/fixtures/arrow-block-return-type/expected.js
@@ -1,10 +1,12 @@
+var _this = this;
+
 import t from 'tcomb';
 const f = x => {
   _assert(x, t.String, 'x');
 
   const ret = function (x) {
     return x;
-  }.call(this, x);
+  }.call(_this, x);
 
   _assert(ret, t.String, 'return value');
 
